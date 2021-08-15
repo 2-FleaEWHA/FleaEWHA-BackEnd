862ae01eeef4539bcec327c05b09b3ad5ff46ac7
Seoyeong Han <68267278+syoung622@users.noreply.github.com>
2021-08-15 13:41:49 +0900

    Revert "Feat:login/main api"

--- fleaewha/src/main/java/kr/or/epro/fleaewha/Dao/MainDaoSqls.java
+++
@@ -1,6 +0,0 @@
-package kr.or.epro.fleaewha.Dao;
-
-public class MainDaoSqls {
-
-	public static final String SELECT_DATA="SELECT title, price, createTime FROM Product ORDER BY createTime DESC limit 12";
-}
