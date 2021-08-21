0e7fcff24e027851d931dcbdf86662385b8a9c64
hyunk-go <78598447+hyunk-go@users.noreply.github.com>
2021-08-21 18:41:37 +0900

    feat:mypage추가

--- fleaewha/src/main/java/kr/or/epro/fleaewha/dto/MypageDto.java
+++ fleaewha/src/main/java/kr/or/epro/fleaewha/dto/MypageDto.java
@@ -2,27 +2,37 @@ package kr.or.epro.fleaewha.dto;
 
 public class MypageDto {
 
-	private String accountID;
 	private String title;
 	private int productID;
-	public int getProductID() {
-		return productID;
-	}
-	public void setProductID(int productID) {
-		this.productID = productID;
-	}
-	public String getAccountID() {
-		return accountID;
-	}
-	public void setAccountID(String accountID) {
-		this.accountID = accountID;
-	}
+	private String name;
+	private String email;
+
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 	
 	
 }
