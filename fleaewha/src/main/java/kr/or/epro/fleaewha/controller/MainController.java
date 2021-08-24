ffd76e0fea94324e30f9989a05455888b8f6bdfa
hyunk-go <78598447+hyunk-go@users.noreply.github.com>
2021-08-24 09:23:54 +0900

    feat:session에 id저장한것 추가

--- fleaewha/src/main/java/kr/or/epro/fleaewha/controller/MainController.java
+++ fleaewha/src/main/java/kr/or/epro/fleaewha/controller/MainController.java
@@ -1,5 +1,6 @@
 package kr.or.epro.fleaewha.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,8 +45,10 @@ public class MainController {
 	public HashMap login(
 		@RequestParam(value="id")String id,
 		@RequestParam(value="name")String name,
-		@RequestParam(value="email")String email){
+		@RequestParam(value="email")String email,
+		HttpSession session){
 		
+		session.setAttribute("id", id);
 		MemberDto dto=new MemberDto();
 		dto.setId(id);
 		dto.setName(name);
@@ -61,6 +65,12 @@ public class MainController {
 			list.put("msg", "fail");
 			return list;
 		}
+	}
+	
+	@RequestMapping(value="/logout")
+	public void logout(HttpSession session)throws IOException{
+		session.invalidate();
+		
 	}
 }
 
