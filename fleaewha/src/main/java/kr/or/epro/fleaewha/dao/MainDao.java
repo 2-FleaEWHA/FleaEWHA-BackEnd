9164fdc6a95760bdc1b8a59140d26e225ca98d99
hyunk-go <78598447+hyunk-go@users.noreply.github.com>
2021-08-30 01:54:08 +0900

    feat:mypage수정

--- fleaewha/src/main/java/kr/or/epro/fleaewha/dao/MainDao.java
+++ fleaewha/src/main/java/kr/or/epro/fleaewha/dao/MainDao.java
@@ -6,10 +6,14 @@ import java.util.Map;
 import kr.or.epro.fleaewha.dto.MainDto;
 import kr.or.epro.fleaewha.dto.MemberDto;
 import kr.or.epro.fleaewha.dto.MypageDto;
+import kr.or.epro.fleaewha.dto.UserDto;
+
 
 public interface MainDao {
 	List<MainDto> getMainData();
 	List<MypageDto> getMyData(String id);
 	public void insertUser(MemberDto dto);
 	boolean checkUser(String id);
+	boolean idcheck(String id);
+	List<UserDto> getUserData(String id);
 }
